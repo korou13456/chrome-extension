831f6e61e076ce53c7edf0792b66c7f8e1d53173
Zane <982021862@qq.com>
2023-09-16 15:03:24 +0800

    fix

--- src/extension/utils/axios.js
+++ src/extension/utils/axios.js
@@ -2,36 +2,33 @@
 
 import axios from 'axios';
 
-const DEV_BASE_URL = 'http://127.0.0.1:8085/api-go'; // 替换为实际的 API 地址
-// const BASE_URL = 'https://backendnew.coupert.com/api-go'; // 替换为实际的 API 地址
+const DEV_BASE_URL = 'http://127.0.0.1:8085/api-go';
+// const BASE_URL = 'https://backendnew.coupert.com/api-go';
 
 const api = axios.create({
     baseURL: DEV_BASE_URL,
-    timeout: 10000, // 请求超时时间
+    timeout: 10000,
 });
 
-// GET 请求示例
 export const get = (url, params = {}) => {
     return api.get(url, { params });
 };
-// POST 请求示例（支持 FormData 格式）
 export const postFormData = (url, formData) => {
     const data = new FormData();
     data.append('name', formData.name);
     return api.post(url, data, {
         headers: {
-            'Content-Type': 'multipart/form-data', // 设置请求头为 FormData
+            'Content-Type': 'multipart/form-data',
         },
     });
 };
 
-// POST 请求示例（支持 FormData 格式）
 export const postUpload = (url, file) => {
     const data = new FormData();
     data.append('file', file);
     return api.post(url, data, {
         headers: {
-            'Content-Type': 'multipart/form-data', // 设置请求头为 FormData
+            'Content-Type': 'multipart/form-data',
         },
     });
 };
